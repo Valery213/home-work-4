68a5cff15889c7da85fde57998a9ca475b6bdd9d
Valery213 <78694775+Valery213@users.noreply.github.com>
2021-03-30 13:01:16 +0300

    Add files via upload

--- home-work-4-1.js
+++ home-work-4-1.js
@@ -1,25 +1,35 @@
+const showMessage = (sum, i, computationHistory) => {
+	alert('Средняя зп будет равна: ' + sum / i + '. \nИстория вычислений: ' + computationHistory.slice(0, -2) + '\nСумма: ' + sum + '.')
+}
 let sum = 0
 let computationHistory = ''
-for (let i = 0; i < 12; i++) {
-    let message = ''
-    if (i === 0) {
-        message = 'Ведите значение заработной платы';
-    } else {
-        message = 'Средняя зп будет равна: ' + sum / i + '. \nИстория вычислений: ' + computationHistory.slice(0, -2) + '\nСумма: ' + sum + '.\nВедите значение заработной платы';
-    }
-    let wage = prompt (message)
-    if (wage === '' || isNaN(wage)) {
-        if(confirm('Невалидное значение. Выйти из приложения?')){
-            break
-        }
-        i--
-    } else {
-        if (wage === null) {
-            break
-        }
-        computationHistory += wage + 'рублей + ';
-        wage = Number(wage)
-        sum += wage;
-    }
+const calculationOfWages = function (i) {
+	if (i === 0) {
+		message = 'Ведите значение заработной платы';
+	} else {
+		message = 'Средняя зп будет равна: ' + sum / i + '. \nИстория вычислений: ' + computationHistory.slice(0, -2) + '\nСумма: ' + sum + '.\nВедите значение заработной платы';
+	}
+	
+	let wage = prompt (message)
+	
+	if (wage === '' || isNaN(wage)) {
+		if(confirm('Невалидное значение. Выйти из приложения?')){
+			showMessage (sum, i, computationHistory)
+			return
+		}
+		i--
+	} else {
+		
+		if (wage === null) {
+			showMessage (sum, i, computationHistory)
+			return
+		}
+		
+		computationHistory += wage + 'рублей + ';
+		wage = Number(wage)
+		sum += wage;
+	}
+	i++;
+	calculationOfWages(i);
 }
-alert('Средняя зп будет равна: ' + sum / 12 + '. \nИстория вычислений: ' + computationHistory.slice(0, -2) + '\nСумма: ' + sum + '.')
+calculationOfWages (0);
